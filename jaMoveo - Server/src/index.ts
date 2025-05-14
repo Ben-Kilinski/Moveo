19387ffe19cf5db0c5a9725f3818aa74ecd47337
Ben Kilinski <ben.kilinski@gmail.com>
2025-05-14 11:22:50 +0300

    index/auth

--- jaMoveo - Server/src/index.ts
+++ jaMoveo - Server/src/index.ts
@@ -1,14 +1,15 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-
+import authRoutes from './routes/auth';
 
 dotenv.config();
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
 
 app.get('/', (_: Request, res: Response) => {
   res.send('Server is running 🚀');
