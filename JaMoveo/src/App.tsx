f4c46ab50df0e5457b56601ac055669f5a3d72c4
Ben Kilinski <ben.kilinski@gmail.com>
2025-05-14 10:39:50 +0300

    Hello world React/TS/Vite/Tailwind

---
+++ JaMoveo/src/App.tsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+
+
+  return (
+    <>
+   <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind funcionando! 🚀
+      </h1>
+    </div>    
+    </>
+  )
+}
+
+export default App
